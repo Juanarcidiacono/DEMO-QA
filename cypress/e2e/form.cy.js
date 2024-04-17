e36ec3de15a95c061f2381ae38467813c42548d1
juan <juanyarcidiacono@gmail.com>
2024-04-17 15:52:27 -0300

    Update form.cy.js

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,7 +1,7 @@
 import urls from '../fixtures/urls.json'
 import Action from '/cypress/actions/Actions.js'
 
-describe("Demo QA",() => {
+describe("Demo QA - Form",() => {
 
     it("Fill form",() => {
        Action.visit(urls.form)
