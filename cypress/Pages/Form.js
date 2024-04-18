01d3372c0727b2739d49f2bc8df3eb4cc5558a73
Juan Arcidiacono <61525466+Juanarcidiacono@users.noreply.github.com>
2024-04-18 13:49:12 -0300

    Update Form.js

--- cypress/Pages/Form.js
+++ cypress/Pages/Form.js
@@ -1,5 +1,27 @@
+import Utils from "?cypress/support/Utils.js"
 class Form {
+
+    elements = {
+        firstName: () => Action.get("#firstName"),
+        lastName: () => Action.get("#lastName"),
+        email: () => Action.get("#userEmail"),
+        gender: () => Action.get("#genterWrapper"),
+        mobileNumber: () => Action.get("#userNumber"),
+        dateBirth: () => Action.get("#dateOfBirth"),
+        subjects: () => Action.get("#subjectsWrapper"),
+        hobbies: () => Action.get("#hobbiesWrapper"),
+        uploadPicture: () => Action.get("#uploadPicture"),
+        currentAddress: () => Action.get("#currentAddress"),
+        state: () => Action.get("#stateCity-wrapper"),
+        submit: () => Action.get("#submit")
+    }
+
+    constants = {
+        'male': 'male',
+        'female': 'female',
+        'other': 'other',
+    }
 
 }
 
-module.exports = new Actions();
+module.exports = new Form();
